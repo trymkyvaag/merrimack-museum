2c590568c19304261c023b99beb1a4d6adc15ba5
Moses Mwaura <moses@Mosess-MacBook-Pro.local>
2023-09-30 18:06:30 -0400

    Installed mantine requirements, added configuration to postcss.config.js and added the mantine provider to rootlayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '@mantine/core/styles.css';
+
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-            {children}
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
   )
