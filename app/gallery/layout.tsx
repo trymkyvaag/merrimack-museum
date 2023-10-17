e482b8b42a376951f2ee4d8bcc8b9a4a1e51840e
dannemora <mmwaura@dannemora.io>
2023-10-16 23:49:03 -0400

    Added a drop down

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -1,21 +1,86 @@
+'use client'
 import type { Metadata } from 'next'
+import Link from 'next/link';
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
+import { Group, Box, Collapse, ThemeIcon, Text, UnstyledButton, rem } from '@mantine/core';
+import { IconCalendarStats, IconChevronRight } from '@tabler/icons-react';
+import classes from '@/styles/NavbarLinksGroup.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: '',
-  description: '',
+interface LinksGroupProps {
+    icon: React.FC<any>;
+    label: string;
+    initiallyOpened?: boolean;
+    links?: { label: string; link: string }[];
 }
 
+export function LinksGroup({ icon: Icon, label, initiallyOpened, links }: LinksGroupProps) {
+    const hasLinks = Array.isArray(links);
+    const [opened, setOpened] = useState(initiallyOpened || false);
+    const items = (hasLinks ? links : []).map((link) => (
+        <Text<'a'>
+            component="a"
+            className={classes.link}
+            href={link.link}
+            key={link.label}
+            onClick={(event) => event.preventDefault()}
+        >
+            {link.label}
+        </Text>
+    ));
+
+    return (
+        <>
+            <UnstyledButton onClick={() => setOpened((o) => !o)} className={classes.control}>
+                <Group justify="space-between" gap={0}>
+                    <Box style={{ display: 'flex', alignItems: 'center' }}>
+                        <ThemeIcon variant="light" size={30}>
+                            <Icon style={{ width: rem(18), height: rem(18) }} />
+                        </ThemeIcon>
+                        <Box ml="md">{label}</Box>
+                    </Box>
+                    {hasLinks && (
+                        <IconChevronRight
+                            className={classes.chevron}
+                            stroke={1.5}
+                            style={{
+                                width: rem(16),
+                                height: rem(16),
+                                transform: opened ? 'rotate(-90deg)' : 'none',
+                            }}
+                        />
+                    )}
+                </Group>
+            </UnstyledButton>
+            {hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
+        </>
+    );
+}
+
+const mockdata = {
+    label: 'Menu',
+    icon: IconCalendarStats,
+    links: [
+        { label: 'Upcoming releases', link: '/' },
+        { label: 'Previous releases', link: '/' },
+        { label: 'Releases schedule', link: '/' },
+    ],
+};
+
+
 export default function Layout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <>
-        <main>{children}</main>
-    </>
-  )
+    return (
+        <>
+            <Box mih={220} p="md">
+                <LinksGroup {...mockdata} />
+            </Box>
+            <main>{children}</main>
+        </>
+    )
 }
