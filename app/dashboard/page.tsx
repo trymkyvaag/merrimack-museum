be71f729ce47ebcc1cdeffac30515520ef491a8b
mwauram <mmwaura@dannemora.io>
2023-11-01 01:12:55 -0400

    Moved uploading to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,26 @@
 'use client'
 
-import { Container, Grid, SimpleGrid, Skeleton, rem, Table, ScrollArea, Checkbox, Group, Avatar, Text } from '@mantine/core';
+import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
+import { IconCloudUpload, IconX, IconDownload } from '@tabler/icons-react';
+import { Container, Grid, SimpleGrid, Skeleton, rem, Table, ScrollArea, Checkbox, Group, Avatar, Text, Button, useMantineTheme } from '@mantine/core';
 import cx from 'clsx';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { table_mock_data } from '@/lib/utils';
-import classes from '@/styles/Table.module.css';
+import classes from '@/styles/Dashboard.module.css';
 
 
-const PRIMARY_COL_HEIGHT = rem(window.innerHeight || document.documentElement.clientHeight);
+const PRIMARY_COL_HEIGHT = typeof window !== 'undefined'
+  ? rem(window.innerHeight || document.documentElement.clientHeight)
+  : rem(300);
 
 export default function Dashboard() {
     const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
 
     const [selection, setSelection] = useState(['1']);
+    const theme = useMantineTheme();
+    const openRef = useRef<() => void>(null);
+    const [uploadedImages, setUploadedImages] = useState<File[]>([]);
+
     const toggleRow = (id: string) =>
         setSelection((current) =>
             current.includes(id) ? current.filter((item) => item !== id) : [...current, id]
@@ -41,6 +49,41 @@ export default function Dashboard() {
         );
     });
 
+
+
+    const handleImageUpload = (files: File[]) => {
+        setUploadedImages([...uploadedImages, ...files]);
+    };
+
+    const removeImage = (index: number) => {
+        const newImages = [...uploadedImages];
+        newImages.splice(index, 1);
+        setUploadedImages(newImages);
+    };
+
+    const sendImagesToServer = () => {
+        const formData = new FormData();
+        uploadedImages.forEach((image, index) => {
+            formData.append(`images`, image, `image${index}`);
+        });
+
+        fetch('/your-upload-url/', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log('Images uploaded successfully');
+                    // You can reset the uploadedImages state or take other actions upon successful upload
+                } else {
+                    console.error('Error uploading images');
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+    
     return (
         <>
             <Container fluid my="md">
@@ -73,7 +116,76 @@ export default function Dashboard() {
                             <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
                         </Grid.Col>
                         <Grid.Col span={6}>
-                            <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+                            {/* <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} /> */}
+
+                            <div className={classes.wrapper}>
+                                <Dropzone
+                                    openRef={openRef}
+                                    onDrop={handleImageUpload}
+                                    className={classes.dropzone}
+                                    radius="md"
+                                    accept={['image/*']}
+                                    maxSize={30 * 1024 ** 2}
+                                    multiple // Enable multiple file upload
+                                >
+                                    <div style={{ pointerEvents: 'none' }}>
+                                        <Group justify="center">
+                                            <Dropzone.Accept>
+                                                <IconDownload
+                                                    style={{ width: rem(50), height: rem(50) }}
+                                                    color={theme.colors.blue[6]}
+                                                    stroke={1.5}
+                                                />
+                                            </Dropzone.Accept>
+                                            <Dropzone.Reject>
+                                                <IconX
+                                                    style={{ width: rem(50), height: rem(50) }}
+                                                    color={theme.colors.red[6]}
+                                                    stroke={1.5}
+                                                />
+                                            </Dropzone.Reject>
+                                            <Dropzone.Idle>
+                                                <IconCloudUpload style={{ width: rem(50), height: rem(50) }} stroke={1.5} />
+                                            </Dropzone.Idle>
+                                        </Group>
+
+                                        <Text ta="center" fw={700} fz="lg" mt="xl">
+                                            <Dropzone.Accept>Drop images here</Dropzone.Accept>
+                                            <Dropzone.Reject>Images must be less than 30mb</Dropzone.Reject>
+                                            <Dropzone.Idle>Upload images</Dropzone.Idle>
+                                        </Text>
+                                        <Text ta="center" fz="sm" mt="xs" c="dimmed">
+                                            Drag'n'drop images here to upload. We can accept any image type that is less than 30mb in size.
+                                        </Text>
+                                    </div>
+                                </Dropzone>
+
+                                {/* {uploadedImages.length > 0 && (
+                <div>
+                    {uploadedImages.map((image, index) => (
+                        <div key={index} className={classes.imageContainer}>
+                            <img src={image} alt={`Uploaded Image ${index}`} />
+                            <Button
+                                className={classes.removeButton}
+                                size="xs"
+                                variant="filled"
+                                onClick={() => removeImage(index)}
+                            >
+                                Remove
+                            </Button>
+                        </div>
+                    ))}
+                </div>
+            )} */}
+
+                                <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+                                    Select images
+                                </Button>
+
+                                {/* <Button className={classes.control} size="md" radius="xl" onClick={sendImagesToServer}>
+                Upload Images
+            </Button> */}
+                            </div>
                         </Grid.Col>
                     </Grid>
                 </SimpleGrid>
