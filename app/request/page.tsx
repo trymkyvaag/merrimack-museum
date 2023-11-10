abb322afa68f25d0c8288013be612b63c0844214
mmwaura <mmwaura@dannemora.io>
2023-11-10 17:32:45 -0500

    Conditionally checking whether user can access form

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import Link from 'next/link';
 import { UnstyledButton, Text, TextInput, Textarea, SimpleGrid, Menu, Image, Group, Title, Button, Container, Stepper } from '@mantine/core';
 import { IconChevronDown } from '@tabler/icons-react';
 import { useForm } from '@mantine/form';
+import { useSession } from 'next-auth/react';
+import { useArtwork } from '@/lib/types';
 import classes from '@/styles/Picker.module.css';
 
 import { request_mock_data } from '@/lib/utils';
 
 export default function Request() {
+    const { data: session, status, update } = useSession();
+    const { artwork, map } = useArtwork();
     const [opened, setOpened] = useState(false);
     const [selected, setSelected] = useState(request_mock_data[0]);
     const [active, setActive] = useState(1);
@@ -26,7 +30,6 @@ export default function Request() {
     ));
     const form = useForm({
         initialValues: {
-            name: '',
             email: '',
             source: '',
             destination: '',
@@ -34,133 +37,152 @@ export default function Request() {
             message: '',
         },
         validate: {
-            name: (value) => value.trim().length < 2,
             email: (value) => !/^\S+@\S+$/.test(value),
             subject: (value) => value.trim().length === 0,  // Make sure to add validators for source and destination
         },
     });
 
+
     const handleSubmit = () => {
 
+        fetch('http://localhost:8000/api/move-requests/', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // Include any additional headers if needed
+            },
+            body: JSON.stringify(form.values),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Handle the response data
+                console.log("Response Data:", data);
+            })
+            .catch((error) => {
+                // Handle errors
+                console.error("Error:", error);
+            });
     };
 
+    useEffect(() => {
+        if (session && session.user) {
+            form.setFieldValue('email', session.user.email ?? '');
+        }
+    }, [session]);
     return (
         <>
-            <Container px='lg' py='lg' size='sm'>
-                <form onSubmit={form.onSubmit(() => { })}>
-                    <Title
-                        order={2}
-                        size="h1"
-                        style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
-                        fw={900}
-                        ta="center"
-                    >
-                        Request a piece
-                    </Title>
+            {
+                // session && session.user 
+                true ?
+                    <div>
+                        <Container px='lg' py='lg' size='sm'>
+                            <form onSubmit={form.onSubmit(() => { })}>
+                                <Title
+                                    order={2}
+                                    size="h1"
+                                    style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
+                                    fw={900}
+                                    ta="center"
+                                >
+                                    Request a piece
+                                </Title>
 
-                    <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
-                        <TextInput
-                            label="Name"
-                            placeholder="Your name"
-                            name="name"
-                            variant="filled"
-                            {...form.getInputProps('name')}
-                        />
-                        <TextInput
-                            label="Email"
-                            placeholder="Your email"
-                            name="email"
-                            variant="filled"
-                            {...form.getInputProps('email')}
-                        />
-                    </SimpleGrid>
+                                <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+                                    <TextInput
+                                        label="Email"
+                                        placeholder="Your email"
+                                        name="email"
+                                        variant="filled"
+                                        {...form.getInputProps('email')}
+                                    />
+                                </SimpleGrid>
 
-                    <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
-                        <TextInput
-                            label="Source"
-                            placeholder="Art Center"
-                            name="source"
-                            variant="filled"
-                            {...form.getInputProps('source')}
-                        />
-                        <TextInput
-                            label="Destination"
-                            placeholder="Obrien ..."
-                            name="destination"
-                            variant="filled"
-                            {...form.getInputProps('destination')}
-                        />
-                    </SimpleGrid>
+                                <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+                                    <TextInput
+                                        label="Source"
+                                        placeholder="Art Center"
+                                        name="source"
+                                        variant="filled"
+                                        {...form.getInputProps('source')}
+                                    />
+                                    <TextInput
+                                        label="Destination"
+                                        placeholder="Obrien ..."
+                                        name="destination"
+                                        variant="filled"
+                                        {...form.getInputProps('destination')}
+                                    />
+                                </SimpleGrid>
 
-                    {/* <TextInput
-                        label="Subject"
-                        placeholder="Subject"
-                        mt="md"
-                        name="subject"
-                        variant="filled"
-                        {...form.getInputProps('subject')}
-                    /> */}
+                                <Menu
+                                    onOpen={() => setOpened(true)}
+                                    onClose={() => setOpened(false)}
+                                    radius="md"
+                                    width="target"
+                                    withinPortal
+                                >
+                                    <Menu.Target>
+                                        <UnstyledButton mt="md" className={classes.control} data-expanded={opened || undefined}>
+                                            <Group gap="xs">
+                                                {/* <Image src={selected.image} width={22} height={22} /> */}
+                                                <span className={classes.label}>{selected.label}</span>
+                                            </Group>
+                                            <IconChevronDown size="1rem" className={classes.icon} stroke={1.5} />
+                                        </UnstyledButton>
+                                    </Menu.Target>
+                                    <Menu.Dropdown>{items}</Menu.Dropdown>
+                                </Menu>
+                                <Textarea
+                                    mt="md"
+                                    label="Message"
+                                    placeholder="Your message"
+                                    maxRows={10}
+                                    minRows={5}
+                                    autosize
+                                    name="message"
+                                    variant="filled"
+                                    {...form.getInputProps('message')}
+                                />
 
-                    <Menu
-                        onOpen={() => setOpened(true)}
-                        onClose={() => setOpened(false)}
-                        radius="md"
-                        width="target"
-                        withinPortal
-                    >
-                        <Menu.Target>
-                            <UnstyledButton mt="md" className={classes.control} data-expanded={opened || undefined}>
-                                <Group gap="xs">
-                                    {/* <Image src={selected.image} width={22} height={22} /> */}
-                                    <span className={classes.label}>{selected.label}</span>
+                                <Group justify="center" mt="xl">
+                                    {/* <Link href="/gallery" passHref> */}
+                                    <Button type="submit" size="md" onSubmit={() => handleSubmit}>
+                                        Submit Request
+                                    </Button>
+                                    {/* </Link> */}
                                 </Group>
-                                <IconChevronDown size="1rem" className={classes.icon} stroke={1.5} />
-                            </UnstyledButton>
-                        </Menu.Target>
-                        <Menu.Dropdown>{items}</Menu.Dropdown>
-                    </Menu>
-                    <Textarea
-                        mt="md"
-                        label="Message"
-                        placeholder="Your message"
-                        maxRows={10}
-                        minRows={5}
-                        autosize
-                        name="message"
-                        variant="filled"
-                        {...form.getInputProps('message')}
-                    />
+                            </form>
+                        </Container>
 
-                    <Group justify="center" mt="xl">
-                        {/* <Link href="/gallery" passHref> */}
-                        <Button type="submit" size="md" onSubmit={() => handleSubmit}>
-                            Submit Request
-                        </Button>
-                        {/* </Link> */}
-                    </Group>
-                </form>
-            </Container>
-
-            <Container py='lg' size='sm'>
-                <Stepper active={active} onStepClick={setActive}>
-                    <Stepper.Step label="First step" description="Request">
-                        Step 1: Submit Request
-                    </Stepper.Step>
-                    <Stepper.Step label="Second step" description="Request Review">
-                        Step 2: Request is being reviewed
-                    </Stepper.Step>
-                    <Stepper.Step label="Final step" description="Approved">
-                        Step 3: Request Approved
-                    </Stepper.Step>
-                    <Stepper.Completed>
-                        Art piece out for delivery
-                    </Stepper.Completed>
-                </Stepper>
-                <Group justify="center" mt="xl">
-                    <Button variant="default" onClick={prevStep}>Back</Button>
-                    <Button onClick={nextStep}>Next step</Button>
-                </Group>
-            </Container>
+                        <Container py='lg' size='sm'>
+                            <Stepper active={active} onStepClick={setActive}>
+                                <Stepper.Step label="First step" description="Request">
+                                    Step 1: Submit Request
+                                </Stepper.Step>
+                                <Stepper.Step label="Second step" description="Request Review">
+                                    Step 2: Request is being reviewed
+                                </Stepper.Step>
+                                <Stepper.Step label="Final step" description="Approved">
+                                    Step 3: Request Approved
+                                </Stepper.Step>
+                                <Stepper.Completed>
+                                    Art piece out for delivery
+                                </Stepper.Completed>
+                            </Stepper>
+                            <Group justify="center" mt="xl">
+                                <Button variant="default" onClick={prevStep}>Back</Button>
+                                <Button onClick={nextStep}>Next step</Button>
+                            </Group>
+                        </Container>
+                    </div>
+                    :
+                    <div><p>Operation not allowed</p></div>
+            }
         </>
     )
 }
